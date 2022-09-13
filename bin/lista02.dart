84de810ca942958be28f710ab6ff8ad439ab7d58
sam <pflegermendes@gmail.com>
2022-09-13 19:53:22 -0300

    first commit

---
+++ bin/lista02.dart
@@ -0,0 +1,5 @@
+import 'package:lista02/lista02.dart' as lista02;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lista02.calculate()}!');
+}
---
+++ test/lista02_test.dart
@@ -0,0 +1,8 @@
+import 'package:lista02/lista02.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
